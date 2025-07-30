fb4efeac7e7cb6c0cd4732afe00e5cbdc869f3d6
Eksonurit <fizervitalij897@gmail.com>
2025-07-30 16:48:22 +0300

    fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,162 +11,184 @@ let eventSalaryClick = 0;
 
 const sortByName = () => {
   eventNameClick++;
+
   const employersList = tbody.querySelectorAll('tr');
 
   const sortedByASC = Array.from(employersList).sort((emloyer1, emloyer2) => {
     const name1 = emloyer1.children[0].textContent;
     const name2 = emloyer2.children[0].textContent;
-    return name1.localeCompare(name2)
+
+    return name1.localeCompare(name2);
   });
 
   const sortedByDESC = Array.from(employersList).sort((emloyer1, emloyer2) => {
     const name1 = emloyer1.children[0].textContent;
     const name2 = emloyer2.children[0].textContent;
-    return name2.localeCompare(name1)
+
+    return name2.localeCompare(name1);
   });
 
   tbody.innerHTML = '';
 
   if (eventNameClick % 2 === 0) {
-    for(const newPerson of sortedByDESC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByDESC) {
+      tbody.append(newPerson);
     }
   } else {
-    for(const newPerson of sortedByASC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByASC) {
+      tbody.append(newPerson);
     }
   }
-}
-
+};
 
 const sortByPosition = () => {
   const employersList = tbody.querySelectorAll('tr');
+
   eventPositionClick++;
 
   const sortedByASC = Array.from(employersList).sort((emloyer1, emloyer2) => {
     const position1 = emloyer1.children[1].textContent;
     const position2 = emloyer2.children[1].textContent;
-    return position1.localeCompare(position2)
+
+    return position1.localeCompare(position2);
   });
 
   const sortedByDESC = Array.from(employersList).sort((emloyer1, emloyer2) => {
     const position1 = emloyer1.children[1].textContent;
     const position2 = emloyer2.children[1].textContent;
-    return position2.localeCompare(position1)
+
+    return position2.localeCompare(position1);
   });
 
   tbody.innerHTML = '';
 
   if (eventPositionClick % 2 === 0) {
-    for(const newPerson of sortedByDESC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByDESC) {
+      tbody.append(newPerson);
     }
   } else {
-    for(const newPerson of sortedByASC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByASC) {
+      tbody.append(newPerson);
     }
   }
-}
+};
 
 const sortByOffice = () => {
   const employersList = tbody.querySelectorAll('tr');
+
   eventOfficeClick++;
 
   const sortedByASC = Array.from(employersList).sort((emloyer1, emloyer2) => {
     const office1 = emloyer1.children[2].textContent;
     const office2 = emloyer2.children[2].textContent;
-    return office1.localeCompare(office2)
+
+    return office1.localeCompare(office2);
   });
 
   const sortedByDESC = Array.from(employersList).sort((emloyer1, emloyer2) => {
     const office1 = emloyer1.children[2].textContent;
     const office2 = emloyer2.children[2].textContent;
-    return office2.localeCompare(office1)
+
+    return office2.localeCompare(office1);
   });
 
   tbody.innerHTML = '';
 
   if (eventOfficeClick % 2 === 0) {
-    for(const newPerson of sortedByDESC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByDESC) {
+      tbody.append(newPerson);
     }
   } else {
-    for(const newPerson of sortedByASC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByASC) {
+      tbody.append(newPerson);
     }
   }
-}
+};
 
 const sortByAge = () => {
   const employersList = tbody.querySelectorAll('tr');
+
   eventAgeClick++;
 
   const sortedByASC = Array.from(employersList).sort((emloyer1, emloyer2) => {
     const age1 = emloyer1.children[3].textContent;
     const age2 = emloyer2.children[3].textContent;
+
     return age1 - age2;
   });
 
   const sortedByDESC = Array.from(employersList).sort((emloyer1, emloyer2) => {
     const age1 = emloyer1.children[3].textContent;
     const age2 = emloyer2.children[3].textContent;
+
     return age2 - age1;
   });
 
   tbody.innerHTML = '';
 
   if (eventAgeClick % 2 === 0) {
-    for(const newPerson of sortedByDESC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByDESC) {
+      tbody.append(newPerson);
     }
   } else {
-    for(const newPerson of sortedByASC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByASC) {
+      tbody.append(newPerson);
     }
   }
-}
+};
 
 const sortBySalary = () => {
   const employersList = tbody.querySelectorAll('tr');
+
   eventSalaryClick++;
 
   const sortedByASC = Array.from(employersList).sort((emloyer1, emloyer2) => {
-    const salary1 = emloyer1.children[4].textContent.replace('$', '').replace(/,/g, '');
-    const salary2 = emloyer2.children[4].textContent.replace('$', '').replace(/,/g, '');
+    const salary1 = emloyer1.children[4].textContent
+      .replace('$', '')
+      .replace(/,/g, '');
+    const salary2 = emloyer2.children[4].textContent
+      .replace('$', '')
+      .replace(/,/g, '');
+
     return Number(salary1) - Number(salary2);
   });
 
   const sortedByDESC = Array.from(employersList).sort((emloyer1, emloyer2) => {
-    const salary1 = emloyer1.children[4].textContent.replace('$', '').replace(/,/g, '');
-    const salary2 = emloyer2.children[4].textContent.replace('$', '').replace(/,/g, '');
+    const salary1 = emloyer1.children[4].textContent
+      .replace('$', '')
+      .replace(/,/g, '');
+    const salary2 = emloyer2.children[4].textContent
+      .replace('$', '')
+      .replace(/,/g, '');
+
     return Number(salary2) - Number(salary1);
   });
 
   if (eventSalaryClick % 2 === 0) {
-    for(const newPerson of sortedByDESC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByDESC) {
+      tbody.append(newPerson);
     }
   } else {
-    for(const newPerson of sortedByASC) {
-    tbody.append(newPerson);
+    for (const newPerson of sortedByASC) {
+      tbody.append(newPerson);
     }
   }
-}
+};
 
 const activePerson = (person) => {
   const employers = tbody.querySelectorAll('tr');
-  const isActive =  Array.from(employers).find((employer) => {
+  const isActive = Array.from(employers).find((employer) => {
     return employer.classList.contains('active');
-  })
+  });
 
   if (isActive) {
-    return
   } else {
     person.classList.add('active');
   }
-}
+};
 
 const form = document.createElement('form');
+
 form.classList.add('new-employee-form');
 
 const nameLable = document.createElement('label');
@@ -182,26 +204,32 @@ const ageInput = document.createElement('input');
 const salaryInput = document.createElement('input');
 
 const optionTokyo = document.createElement('option');
+
 optionTokyo.textContent = 'Tokyo';
 optionTokyo.value = 'Tokyo';
 
 const optionSingapore = document.createElement('option');
+
 optionSingapore.textContent = 'Singapore';
 optionSingapore.value = 'Singapore';
 
 const optionLondon = document.createElement('option');
+
 optionLondon.textContent = 'London';
 optionLondon.value = 'London';
 
 const optionNewYork = document.createElement('option');
+
 optionNewYork.textContent = 'New York';
 optionNewYork.value = 'New York';
 
 const optionEdinburgh = document.createElement('option');
+
 optionEdinburgh.textContent = 'Edinburgh';
 optionEdinburgh.value = 'Edinburgh';
 
 const optionSanFrancisco = document.createElement('option');
+
 optionSanFrancisco.textContent = 'San Francisco';
 optionSanFrancisco.value = 'San Francisco';
 
@@ -211,25 +239,21 @@ officeInput.append(
   optionLondon,
   optionNewYork,
   optionEdinburgh,
-  optionSanFrancisco
+  optionSanFrancisco,
 );
 
 const formButton = document.createElement('button');
+
 formButton.textContent = 'Save to table';
 formButton.type = 'submit';
 
 nameInput.setAttribute('name', 'name');
 positionInput.setAttribute('name', 'position');
 officeInput.setAttribute('name', 'office');
-officeInput.setAttribute('data-qa', 'name');
-officeInput.setAttribute('data-qa', 'position');
 officeInput.setAttribute('data-qa', 'office');
-officeInput.setAttribute('data-qa', 'age');
-officeInput.setAttribute('data-qa', 'salary');
 officeInput.required = true;
 ageInput.setAttribute('name', 'age');
 salaryInput.setAttribute('name', 'salary');
-
 
 nameInput.setAttribute('type', 'text');
 nameInput.setAttribute('data-qa', 'name');
@@ -262,16 +286,24 @@ officeLable.append(officeInput);
 ageLable.append(ageInput);
 salaryLable.append(salaryInput);
 
-form.append(nameLable, positionLable, officeLable, ageLable, salaryLable, formButton);
-
+form.append(
+  nameLable,
+  positionLable,
+  officeLable,
+  ageLable,
+  salaryLable,
+  formButton,
+);
 
 body.append(form);
 
 const notification = document.createElement('div');
+
 notification.className = 'notification';
 notification.style.display = 'none';
 
 const notificationTitle = document.createElement('span');
+
 notificationTitle.className = 'title';
 notification.append(notificationTitle);
 
@@ -279,7 +311,10 @@ body.append(notification);
 
 function showNotification(type = '', message = '') {
   notification.className = 'notification';
-  if (type) notification.classList.add(type);
+
+  if (type) {
+    notification.classList.add(type);
+  }
   notification.style.display = 'block';
 
   if (type === 'success') {
@@ -303,34 +338,38 @@ function showNotification(type = '', message = '') {
   }, 2000);
 }
 
-
 const addEmployer = () => {
   const newEmployer = document.createElement('tr');
   const employerName = document.createElement('td');
+
   employerName.textContent = nameInput.value;
 
   const employerPosition = document.createElement('td');
+
   employerPosition.textContent = positionInput.value;
 
   const employerOffice = document.createElement('td');
+
   employerOffice.textContent = officeInput.value;
 
   const employerAge = document.createElement('td');
+
   employerAge.textContent = ageInput.value;
 
-  const employerSlary = document.createElement('td');
+  const employerSalary = document.createElement('td');
   const salary = Number(salaryInput.value).toLocaleString('en-US');
-  employerSlary.textContent = '$' + salary;
 
-  newEmployer.append(employerName,
+  employerSalary.textContent = '$' + salary;
+
+  newEmployer.append(
+    employerName,
     employerPosition,
     employerOffice,
     employerAge,
-    employerSlary
-  )
+    employerSalary,
+  );
   tbody.append(newEmployer);
-}
-
+};
 
 document.addEventListener('click', (e) => {
   const tr = e.target.closest('tr');
@@ -349,10 +388,15 @@ document.addEventListener('click', (e) => {
     activePerson(tr);
   } else if (e.target.tagName === 'BUTTON') {
     e.preventDefault();
-    if (nameInput.value.length < 4 || Number(ageInput.value) < 18 || Number(ageInput.value) > 90) {
+
+    if (
+      nameInput.value.length < 4 ||
+      Number(ageInput.value) < 18 ||
+      Number(ageInput.value) > 90
+    ) {
       showNotification('error');
     } else {
-      addEmployer()
+      addEmployer();
       showNotification('success');
     }
   }
